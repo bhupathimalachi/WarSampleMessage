2192ce24cfe4c10bf2a7e95d619ccfeed15d2686
bhupathimalachi <bhupathim2018@gmail.com>
2021-11-29 21:31:54 +0530

    WarSampleMessage

---
+++ src/main/java/in/bhupathi/controller/WelcomeController.java
@@ -0,0 +1,19 @@
+package in.bhupathi.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class WelcomeController {
+	
+	@GetMapping("/welcome")
+	public ModelAndView displayWelcomeMsg() {
+		ModelAndView mav = new ModelAndView();
+		mav.addObject("msg", "Welcome to Jeswica Computer Education");
+		mav.setViewName("index");
+		return mav;
+		
+	}
+
+}
